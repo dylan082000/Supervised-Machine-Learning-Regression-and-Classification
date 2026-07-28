644f4c51fba81410a717cf4bc1ac862f41fc385f
Dylan <dylannn08200@gmail.com>
2026-07-29 01:03:08 +0700

    day2 study about variable

---
+++ python/day2.py
@@ -0,0 +1,31 @@
+#day 2
+
+first_name = "John"
+age = 20
+is_married = True
+skills = ["Python", "JavaScript", "SQL"]
+person_info = {
+    "first_name": "John",
+    "age": 20,
+    "is_married": True,
+    "skills": ["Python", "JavaScript", "SQL"]
+}
+
+print(person_info)
+print(person_info["first_name"])
+print(person_info["age"])
+print(person_info["is_married"])
+
+print(skills)
+print(skills[0])
+print(skills[1])
+print(skills[2])
+
+print(person_info["skills"])
+print(person_info["skills"][0])
+print(person_info["skills"][1])
+print(person_info["skills"][2])
+
+print(person_info["skills"][0])
+
+first_name, last_name, age, country, city = "John", "Doe", 20, "USA", "New York"
